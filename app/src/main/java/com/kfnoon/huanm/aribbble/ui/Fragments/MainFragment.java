f099443743c7865cdb52f5c0fa115e0ea0d480a5
MKFMIKU <mikumkf@outlook.com>
2017-01-18 13:45:16 +0800

    Import DrawLayout

--- app/src/main/java/com/kfnoon/huanm/aribbble/ui/Fragments/MainFragment.java
+++ app/src/main/java/com/kfnoon/huanm/aribbble/ui/Fragments/MainFragment.java
@@ -1,29 +1,33 @@
-package com.kfnoon.huanm.aribbble;
+package com.kfnoon.huanm.aribbble.ui.Fragments;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.kfnoon.huanm.aribbble.R;
 import com.kfnoon.huanm.aribbble.adapter.ShotsAdapter;
 import com.kfnoon.huanm.aribbble.api.BaseClient;
 import com.kfnoon.huanm.aribbble.model.Shot;
 import com.kfnoon.huanm.aribbble.ui.ShotActivity;
 
-import android.content.Intent;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-
 import java.util.ArrayList;
 import java.util.List;
+
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainFragment extends Fragment {
     private ProgressBar loadingView;
     private RecyclerView mRecyclerView;
     private ShotsAdapter shotsAdapter;
@@ -34,19 +38,26 @@ public class MainActivity extends AppCompatActivity {
     private int pages;
     private List<Shot> shotList = new ArrayList<Shot>();
     private Subscription mSubscription;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        loadingView = (ProgressBar) findViewById(R.id.loading);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
-        mRecyclerView = (RecyclerView) findViewById(R.id.shotsList);
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        loadingView = (ProgressBar) view.findViewById(R.id.loading);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshLayout);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.shotsList);
         mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new GridLayoutManager(this, 2);
+        mLayoutManager = new GridLayoutManager(getContext(), 2);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        initData();
+
         loading = false;
+        initData();
 
         swipeRefreshLayout.setEnabled(false);
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
@@ -67,41 +78,37 @@ public class MainActivity extends AppCompatActivity {
                 lastVisible = mLayoutManager.findLastVisibleItemPosition();
             }
         });
+
+        return view;
     }
 
-    private void initUi() {
-        loadingView.setVisibility(View.GONE);
-        shotsAdapter = new ShotsAdapter(getApplicationContext(), shotList);
-        shotsAdapter.getPostionClicks().subscribe(new Action1<Integer>() {
-            @Override
-            public void call(Integer integer) {
-                startActivity(new Intent(MainActivity.this, ShotActivity.class).putExtra("id", integer));
-            }
-        });
-        mRecyclerView.setAdapter(shotsAdapter);
 
-    }
 
     private void initData(){
         pages = 1;
         mSubscription = BaseClient.instance()
                 .getShots(pages)
+                .doOnError(this::handleError)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnError(new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                })
-                .subscribe(new Action1<List<Shot>>() {
-                    @Override
-                    public void call(List<Shot> shots) {
-                        shotList = shots;
-                        initUi();
-                    }
-                });
+                .subscribe(this::bind, t->{});
+    }
 
+    private void bind(List<Shot> shots) {
+        shotList = shots;
+        loadingView.setVisibility(View.GONE);
+        shotsAdapter = new ShotsAdapter(getContext(), shotList);
+        shotsAdapter.getPostionClicks().subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                startActivity(new Intent(getContext(), ShotActivity.class).putExtra("id", integer));
+            }
+        });
+        mRecyclerView.setAdapter(shotsAdapter);
+    }
+
+    private void handleError(Throwable throwable) {
+        Log.d("SingleShot","Failed to load",throwable);
     }
 
     private void updateData(final int postion){
@@ -121,4 +128,5 @@ public class MainActivity extends AppCompatActivity {
                 });
 
     }
+
 }
